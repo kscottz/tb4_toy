a1489ec269fc3139477d800060ad78eb5104d185
kscottz <katherineAScott@gmail.com>
2024-04-21 15:43:16 -0700

    Add node.

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'toy_node = tb4_toy.toy_node:main'
         ],
     },
 )
---
+++ tb4_toy/toy_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from tb4_toy.')
+
+
+if __name__ == '__main__':
+    main()
