f3899df907ba92cdf2c00b64a8e498a11b870332
kscottz <katherineAScott@gmail.com>
2024-05-13 14:05:06 -0700

    Final Toy Node

--- tb4_toy/toy_node.py
+++ tb4_toy/toy_node.py
@@ -1,11 +1,6 @@
 import rclpy
 from rclpy.node import Node
-
-
 from nav_msgs.msg import Odometry
-#from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
-from std_msgs.msg import String
-
 
 class ToyNode(Node):
 
@@ -22,7 +17,6 @@ class ToyNode(Node):
         self.get_logger().info("I'm at ({0},{1})".format(msg.pose.pose.position.x,
                                                          msg.pose.pose.position.y))
 
-
 def main(args=None):
     rclpy.init(args=args)
 
@@ -31,11 +25,8 @@ def main(args=None):
     rclpy.spin(toy_node)
 
     # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     toy_node.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
