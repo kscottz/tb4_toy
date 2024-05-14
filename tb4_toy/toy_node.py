1218f33bd20f52d7accdbd20c04664a96e08986e
kscottz <katherineAScott@gmail.com>
2024-05-13 17:25:54 -0700

    There seems to be three definistions of Trigger.srv

--- tb4_toy/toy_node.py
+++ tb4_toy/toy_node.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from std_srvs.srv import Trigger
 
 class ToyNode(Node):
 
@@ -12,10 +14,26 @@ class ToyNode(Node):
             self.odom_callback,
             10)
         self.subscription  # prevent unused variable warning
-
+        self.position_x = 0.00
+        self.position_y = 0.00
+        self.publisher = self.create_publisher(Twist, '/cmd_vel',10)
+        self.srv = self.create_service(Trigger, 'do_loopy', self.do_loopy_callback)
+        
     def odom_callback(self, msg):
         self.get_logger().info("I'm at ({0},{1})".format(msg.pose.pose.position.x,
                                                          msg.pose.pose.position.y))
+        self.position_x = msg.pose.pose.position.x
+        self.position_y = msg.pose.pose.position.y
+        
+    def do_loopy_callback(self, request, response):
+        self.get_logger().info('Incoming request')
+        self.get_logger().info("Looping at ({0},{1})".format(self.position_x,
+                                                             self.position_y))
+
+        
+
+        return response
+
 
 def main(args=None):
     rclpy.init(args=args)
